fd053ecd5af489bee78cd8611088dba785bd0668
yvanprt <yvan.parent@etu.sorbonne-universite.fr>
2026-05-22 16:16:37 +0200

    better login interface

--- src/login.jsx
+++ src/login.jsx
@@ -17,8 +17,32 @@ export default class Login extends Component {
     }
 
     render() {
-        return this.state.logged
-            ? <a href="/logout">Se déconnecter</a>
-            : <a href="/login.html">Se connecter</a>;
+        const isLogged = this.state.logged;
+
+        return (
+            <div style={{
+                position: 'absolute',
+                top: 10,
+                right: 10,
+                zIndex: 99999
+            }}>
+                <a
+                    href={isLogged ? "/logout" : "/login.html"}
+                    style={{
+                        display: 'inline-block',
+                        padding: '18px 26px',
+                        fontSize: '22px',
+                        fontWeight: 'bold',
+                        border: '2px solid black',
+                        backgroundColor: '#e6e6e6',
+                        textDecoration: 'none',
+                        color: isLogged ? 'red' : '#1a73e8',
+                        cursor: 'pointer'
+                    }}
+                >
+                    {isLogged ? "Se déconnecter" : "Se connecter"}
+                </a>
+            </div>
+        );
     }
 }
